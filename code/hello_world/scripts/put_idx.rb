4ef021b4eaa65fce2f2d1fd0a6b3b6e67cc32919
Jonathanfan <Jonathanfan@campus-068-226.ucdavis.edu>
2018-08-02 17:48:54 -0700

    [MON] Update put_idx.rb

--- code/hello_world/scripts/put_idx.rb
+++ code/hello_world/scripts/put_idx.rb
@@ -7,20 +7,25 @@ client = HTTPClient.new
 config = YAML.load_file('config.yml')
 fed = config['fed']
 
+#Set the IP address of the destination of index registration request
 # base_uri = 'http://xxx.xxx.xxx.xxx:30013'
 base_uri = 'http://' + fed + ':30013'
 
 resource_path = '/sink_agent/api/put_idx'
 header = { 'Content-type' => 'application/json' }
 uri = base_uri + resource_path
-# data_id = "UUID"
-data_id = "041c5476-d1e4-4f29-b385-bb583c86b0ef"
-# [ucdavis@hippos-fed]$ ruby insertion.rb
-#   code=200
-#   {"result":true,"data_id":"041c5476-d1e4-4f29-b385-bb583c86b0ef"}
-# [ucdavis@hippos-fed]$ date
-#   Fri Jul 27 14:54:45 PDT 2018
 
+#Set the data_id acquired in step 4
+# data_id = "UUID"
+data_id = "4332579a-1762-4af9-81a7-a762be3f0f4e"
+
+#[ucdavis@localhost scripts]$ ruby rawdata.rb
+#code=200
+#{"result":true,"data_id":"4332579a-1762-4af9-81a7-a762be3f0f4e"}
+#[ucdavis@localhost scripts]$ date
+#Thu Aug  2 16:58:19 PDT 2018
+
+#Create an index to be registered, make sure to include data_id in sink_info                                                                  
 payload = Hash.new
 payload[:sink_info] = {:data_id => data_id}
 payload[:dir_keys] = ["location", "device_id"]
@@ -32,11 +37,15 @@ payload[:meta] = {
 }
 payload[:gen_time] = Time.now.to_i
 
+#Send index registration request
 res = client.post(uri, payload.to_json, header)
+
+#Get response (idx_id) at index registration
 puts "code=#{res.code}"
 puts res.body
-# [ucdavis@hippos-fed]$ ruby put_idx.rb
-#   code=200
-#   {"result":false,"idx_id":null}
-# [ucdavis@hippos-fed]$ date
-#   Fri Jul 27 14:55:48 PDT 2018
+
+#[ucdavis@localhost scripts]$ ruby put_idx.rb
+#code=200
+#{"result":true,"idx_id":"df9d0d84-9dce-4065-a5c6-0a79ca1ad3e7"}
+#[ucdavis@localhost scripts]$ date
+#Thu Aug  2 17:05:33 PDT 2018
