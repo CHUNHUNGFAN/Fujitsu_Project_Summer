63b84c541109a4f37dab37ad4ab36c30270d1336
Jonathanfan <Jonathanfan@campus-067-183.ucdavis.edu>
2018-08-10 14:33:49 -0700

    [MON] Update directorySearch.rb

--- code/hello_world/scripts/directorySearch.rb
+++ code/hello_world/scripts/directorySearch.rb
@@ -1,6 +1,7 @@
 require 'httpclient'
 require 'json'
 require 'yaml'
+require 'uri'
 
 client = HTTPClient.new
 # Ref. https://stackoverflow.com/questions/21422494/reading-and-updating-yaml-file-by-ruby-code
@@ -9,23 +10,28 @@ loc = config['location']
 
 #Set the IP address of the destination of index registration request
 # base_uri = 'http://xxx.xxx.xxx.xxx:30013'
-base_uri = 'https://api.sys3.iot.jp.fujitsu.com/v1/VTB003-001/testbeddir/'
+base_uri = 'https://api.sys3.iot.jp.fujitsu.com/v1/VTB003-001/'
 
-key = 'keyvalues/'
+resource_path = 'testbeddir/keyvalues/device_id/'
 
-resource_path = 'raw_entries/'
-
-searchkey = 'device_id/'
+# resource_path = 'raw_entries/'
 
 header = { 'Authorization': 'Bearer testbeddirTokyo'}
 
 # uri = base_uri + resource_path + '_past?$filter=gen_time%20gt%20%271533867180%27'
-uri = base_uri + key + searchkey + '_past?$filter=gen_time%20gt%201533867180'
+# uri = base_uri + key + searchkey + '_past?$filter=gen_time gt 1533867180'
+uri = base_uri + resource_path + "_past?$filter=value eq 'dev-01'"
+
+uri = URI.parse(uri)
 
 res = client.get(uri,nil,header)
 
-puts uri
-puts res
+# puts uri
 
-puts "code=#{res.code}"
-puts res.body
+# puts "code=#{res.code}"
+# puts res.body
+
+jsonResult = JSON.parse( res.body )
+
+# data_id = jsonResult[1]
+# puts data_id
